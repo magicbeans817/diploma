b006481a4daef378094ebc1cf1302b5bac3830a6
magicbeans817 <jsuchanek98@gmail.com>
2022-06-07 12:50:55 +0200

    Update script_data_and_modelling.R

--- script_data_and_modelling.R
+++ script_data_and_modelling.R
@@ -12,12 +12,37 @@ setwd("C:/Users/jsuch/Desktop/diploma") #set your working directory
 
 data <- list()
 
-for (i in 1:4) {
+pocet_sheetu <- 4
+
+for (i in 1:pocet_sheetu) {
   data[[i]] <- read_excel("inflace.xlsx", sheet = i)
 }
-data[[1]] %>% head
+
+data[[2]] %>% head
+
+for (i in 1:pocet_sheetu) {
+  print(i)
+  x <- data[[i]]
+  y <- data.frame(x[,-1], row.names = x$Year)
+  data[[i]] <- y
+  
+}
+
+
+
+x <- data.frame(x, row.names = x[,"Year"])
+row.names(x) <- x[,"Year"]
+x %>% dim
+x$Year %>% length
+
+y <- data.frame(x[, -1], row.names = x$Year)
+
 
 tabulka_1 <- data[[1]]
+
+x <- ts(tabulka_1)
+x
+
 x <- c(t(tabulka_1))
 x %>% is.vector
 x
