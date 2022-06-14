24a2a35025caeb6cbf1430a5feaa1bb480e7c984
magicbeans817 <jsuchanek98@gmail.com>
2022-06-14 15:08:36 +0200

    Update script.R

--- script.R
+++ script.R
@@ -50,8 +50,8 @@ inf_cpm     <- data[[3]] %>% t %>% unlist %>% as.vector
 inf_classic <- data[[4]] %>% t %>% unlist %>% as.vector
 
 inf_fd      <- ts(inf_fd, frequency = 12, start = c(2000, 1))
-inf_cmpy    <- ts(inf_cmpy, frequency = 12, start = c(1997, 1))
-inf_cpm     <- ts(inf_cpm, frequency = 12, start = c(1997, 1))
+inf_cmpy    <- ts(inf_cmpy, frequency = 12, start = c(1997, 1)) # inf, corresponding month preceeding year
+inf_cpm     <- ts(inf_cpm, frequency = 12, start = c(1997, 1))  # inf, corresponding preceeding month
 inf_classic <- ts(inf_classic, frequency = 12, start = c(1998, 1))
 
 
@@ -61,6 +61,17 @@ inf_classic <- ts(inf_classic, frequency = 12, start = c(1998, 1))
 gt <- read.csv("search_trends.csv", row.names = 1)
 gt %>% head
 gt%>% dim
+
+
+gt_inf <- ts(gt$inflace, frequency = 12, start = c(2004, 1))
+gt_inf_s <- window(gt_inf, start = start, end = end)
+plot(gt_inf_s)
+tseries::adf.test(gt_inf_s)
+
+
+
+
+
 
 ######################################################################################################
 # 3) Inflation and stationarity
@@ -98,93 +109,6 @@ tseries::adf.test(inf_cpm_s)
 inf_cpm_s_sd <- diff(inf_cpm_s)
 plot(inf_cpm_s_sd)
 tseries::adf.test(inf_cpm_s_sd)
-
-
-
-gt_inf <- ts(gt$inflace, frequency = 12, start = c(2004, 1))
-gt_inf_s <- window(gt_inf, start = start, end = end)
-plot(gt_inf_s)
-tseries::adf.test(gt_inf_s)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-inf_fd_s <- window(inf_fd, start = (2010))
-inf_fd_s %>% length
-
-
-model <- lm(inf_fd_s ~ gt$inflace)
-summary(model)
-plot(inf_fd_s)
-plot(model$fitted)
-lines(inf_fd_s)
-
-modylek <- forecast::Arima(inf_fd, order = c(1, 0, 0))
-summary(modylek)
-
-
-pomocna <- diff(inf_fd)
-modylek2 <- lm(inf_fd[-1] ~ pomocna)
-summary(modylek2)
-
-plot(inf_fd)
-lines(modylek$fitted, col = "red")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-x <- data.frame(x, row.names = x[,"Year"])
-row.names(x) <- x[,"Year"]
-x %>% dim
-x$Year %>% length
-
-y <- data.frame(x[, -1], row.names = x$Year)
-
-
-tabulka_1 <- data[[1]]
-
-
-x <- c(t(tabulka_1))
-x %>% is.vector
-x
-
-
-
-
 
 
 
