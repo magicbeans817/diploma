aa15b8466198267f82b0a45819aac53180859013
magicbeans817 <jsuchanek98@gmail.com>
2023-03-17 14:24:42 +0100

    Update sript2.0.R

--- sript2.0.R
+++ sript2.0.R
@@ -9,7 +9,7 @@ library(rugarch)
 library(xts)
 library(stats)
 library(crayon)
-
+library(vars)
 
 
 rm(list = ls())
@@ -120,7 +120,8 @@ s_inf_cpm_s <- window(ts_deseasonalized, frequency = 12, start = c(rok, mesic), 
 for (i in 1:ncol(gt_dss)) {
   x <- ts(gt_dss[, i], start = start, frequency = 12)
   for (j in 1:3) {
-    lmtest::grangertest(s_inf_cpm_s, x, order = j) %>% print
+    y<- lmtest::grangertest(s_inf_cpm_s, x, order = j)
+    print(y)
     print(colnames(gt_dss)[i])
   }
 }
@@ -136,17 +137,14 @@ for (i in 1:ncol(gt_dss)) {
 
 
 
-
-
 ######################################################################################################
 # Arima
 
-# Load the necessary packages
-library(forecast)
 
 # Convert the data to time series format
 ts_real_inf <- ts(data = s_inf_cpm_s, start = c(2004, 1), frequency = 12)
 
+pocet_ss <- 0
 for (i in 1:ncol(gt_dss)) {
   print(i)
   regresor <- ts(data = gt_dss[,i], start = c(2004, 1), frequency = 12)
@@ -156,8 +154,10 @@ for (i in 1:ncol(gt_dss)) {
   se_coef <- sqrt(diag(arima_model$var.coef))[3]
   co <- arima_model$coef[3]
   ss <- round(co/ se_coef, digits = 2)
-  print(ss)
-  if (abs(ss) > 1.95){
+  p_value <- 2 * (1 - pnorm(abs(ss)))
+  print(p_value)
+  if (p_value < 0.1){
+    pocet_ss <- pocet_ss + 1
     b <- colnames(gt_dss)[i]
     cat(red(b))
     
@@ -174,79 +174,39 @@ for (i in 1:ncol(gt_dss)) {
   }
 }
 
+print(pocet_ss)
 
 
 
 
-ts_gt_inf <- ts(data = s_gt_inf_s, start = c(2004, 1), frequency = 12)
 
-# Fit the ARIMA(1,1,0) model to the data
-arima_model <- forecast::Arima(ts_real_inf, order = c(1,1,1), xreg = gt_dss)
-summary(arima_model)
-
-arima_model <- forecast::Arima(ts_real_inf, order = c(1,1,1))
-summary(arima_model)
-
-acf(arima_model)
-pacf(arima_model)
-
-
-# Generate the fitted values
-fitted_values <- arima_model$fitted
-
-# Plot the actual and fitted values
-plot(ts_real_inf, main = "ARIMA(1,1,0) Fitted Values for Inflation",
-     xlab = "Time", ylab = "Real Inflation")
-lines(fitted_values, col = "red")
-legend("topleft", legend = c("Actual", "Fitted"), lty = c(1,1), col = c("black", "red"))
 
 
 
 ######################################################################################################
-# Load the necessary packages
-library(vars)
-
-# Load the necessary packages
-library(vars)
-
-# Convert the data to time series format
-ts_real_inf <- ts(data = s_inf_cpm_s, start = c(2004, 1), frequency = 12)
-ts_gt_inf <- ts(data = s_gt_inf_s, start = c(2004, 1), frequency = 12)
-
-# Combine the time series into a matrix
-data_matrix <- cbind(ts_real_inf, ts_gt_inf)
-
-complete_matrix <- data_matrix[complete.cases(data_matrix), ]
-
-# Fit a VAR model with lag length 1
-var_model <- vars::VAR(complete_matrix, p = 1, type = "const")
-
-# Print a summary of the model
-summary(var_model)
-
-# Plot the impulse response function for the Google Trends variable
-irf_plot <- plot(vars::irf(var_model, impulse = "ts_gt_inf", response = "ts_real_inf", n.ahead = 12))
+# VAR model
 
 
-# Convert each column of the data frame to a time series and remove seasonality
-for (i in 1:ncol(gt)) {
-  ts_data <- ts(gt[, i], start = c(2000, 1), frequency = 12)
-  decomp_data <- decompose(ts_data)
-  detrended_data <- decomp_data$x #+ decomp_data$trend #trend #seasonal
-  ts_detrended_data <- ts(detrended_data, start = start, end = end, frequency = 12)
-  ts_detrended_data <- ts_detrended_data[-length(ts_detrended_data)]
-  cor_value <- cor(ts_detrended_data, ts_real_inf, use = "pairwise.complete.obs")
-  print(paste0("Correlation for column ", i - 1, ": ", cor_value))
-  print(ts_detrended_data[1:30])
+
+
+for (i in 1:ncol(gt_dss)) {
+  print(i)
+  for (j in 1:1) {
+    data_matrix <- cbind(ts_real_inf, gt_dss[,i])
+    complete_matrix <- data_matrix[complete.cases(data_matrix), ]
+    var_model <- vars::VAR(complete_matrix, p = j, type = "const")
+    summary(var_model) %>% print
+  }
 }
 
 
 
+summary(var_model)
 
 
 
-
-
+# Plot the impulse response function for the Google Trends variable
+irf_plot <- plot(vars::irf(var_model, impulse = "ts_gt_inf", response = "ts_real_inf", n.ahead = 12))
 
 
 
