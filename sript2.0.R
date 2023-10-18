dcbdc4e19ae691a00580cc6e0fabd3def529223b
magicbeans817 <jsuchanek98@gmail.com>
2023-10-18 21:40:58 +0200

    Update sript2.0.R

--- sript2.0.R
+++ sript2.0.R
@@ -901,7 +901,7 @@ modelos
   
 modelos$arma
 
-
+#pro naftu, benzin a pc1 idealni
 ar <- 0
 dif <- 1
 ma <- 1
@@ -921,7 +921,7 @@ if(bench == TRUE){
 
 end   <- c(2023, 2)
 
-regresor <- ts(data = gt_dss[,"cena.benzinu"], start = c(2004, 1), end = end, frequency = 12)
+regresor <- ts(data = gt_dss[,"PC1"], start = c(2004, 1), end = end, frequency = 12)
 regresor <- regresor / mean(regresor) * mean(regresor)
 
 #future_values <- opposite_lag(ext_regressor, 1)
